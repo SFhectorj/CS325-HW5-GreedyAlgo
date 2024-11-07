e50dc7356be3d7f6cb8773e0f92887e226e817bb
github_username <github_email>
2024-11-06 22:08:53 -0800

    homework 5

--- FeedDog.py
+++ FeedDog.py
@@ -1,6 +1,7 @@
 def feedDog(hunger_level, biscuit_size):
     """
-    :param hunger_level:
-    :param biscuit_size:
-    :return:
+    :param hunger_level: array hunger_level [1..n] (ith dog has hunger level of hunger_level [i])
+    :param biscuit_size: dog biscuits of size given by biscuit_size [1…m].
+    This function contains a greedy algorithm that is used to match biscuits to the appropriate
+    hunger level of a dog. The function will return the total number of dogs that can be satisfied.
     """
