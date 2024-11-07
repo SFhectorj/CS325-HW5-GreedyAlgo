4e85111d0e5a447d30663536d007e08ef883997f
github_username <github_email>
2024-11-06 23:39:30 -0800

    homework 5

--- FeedDog.py
+++ FeedDog.py
@@ -5,11 +5,19 @@ def feedDog(hunger_level, biscuit_size):
     This function contains a greedy algorithm that is used to match biscuits to the appropriate
     hunger level of a dog. The function will return the total number of dogs that can be satisfied.
     """
-    # Part 1: sorting
+    # Part 1: Sorting
+    # Both arrays are sorted to make matching the smallest values easier
     hunger_level.sort()
     biscuit_size.sort()
-    
 
+    # Part 2:
+    # Initialization
     total_dogs_satisfied = 0
+    # Keep track of the indices with pointers
+    hunger_pointer = 0
+    biscuit_pointer = 0
+
+    while hunger_pointer < len(hunger_level) and len(biscuit_size):
+
 
     return total_dogs_satisfied
